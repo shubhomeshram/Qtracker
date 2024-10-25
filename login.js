6851834ad1882e8ccb6ad150aa4a38178bc71e5b
shubhomeshram <sameermeshram1192@gmail.com>
2024-10-26 02:08:34 +0530

    Add Intial Files

---
+++ login.js
@@ -0,0 +1,11 @@
+function checkCred(){
+    let cnd = false;
+    if(cnd)
+    {
+        open("homepage.html");
+    }
+    else
+    {
+        alert("Entered Credentials are incorrect")
+    }
+}
